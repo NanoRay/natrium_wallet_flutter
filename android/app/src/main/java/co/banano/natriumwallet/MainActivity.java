bf2bbbfb642ae95540d367f5dd091d4453bdd13e
Alberto Berti <alberto@metapensiero.it>
2019-06-10 22:24:50 +0000

    Add Appia logo to the splash screen

--- android/app/src/main/java/co/banano/natriumwallet/MainActivity.java
+++ android/app/src/main/java/co/banano/natriumwallet/MainActivity.java
@@ -1,6 +1,8 @@
 package co.banano.natriumwallet;
 
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 
 import io.flutter.app.FlutterFragmentActivity;
 import io.flutter.plugin.common.MethodCall;
@@ -15,8 +17,18 @@ public class MainActivity extends FlutterFragmentActivity {
   protected void onCreate(Bundle savedInstanceState) {
     FlutterMain.startInitialization(this);
     super.onCreate(savedInstanceState);
-
     GeneratedPluginRegistrant.registerWith(this);
+    // make transparent status bar
+    getWindow().setStatusBarColor(0x00000000);
+    // Remove full screen flag after load
+    ViewTreeObserver vto = getFlutterView().getViewTreeObserver();
+    vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+      @Override
+      public void onGlobalLayout() {
+        getFlutterView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      }
+    });
 
     new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
           new MethodChannel.MethodCallHandler() {
@@ -31,7 +43,7 @@ public class MainActivity extends FlutterFragmentActivity {
                   } else if (call.method.equals("getLegacyPin")) {
                       result.success(new MigrationStuff().getLegacyPin());
                   } else if (call.method.equals("getSecret")) {
-                      result.success(new LegacyStorage().getSecret());                      
+                      result.success(new LegacyStorage().getSecret());
                   } else {
                       result.notImplemented();
                   }
